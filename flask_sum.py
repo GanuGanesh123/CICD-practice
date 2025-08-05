0f713e6c923f9227248d7298d3a7a51926fd27c1
Sai sri ganesh Putsala <saiganesh.putsala@gmail.com>
2025-08-04 22:24:00 -0400

    added DB requests and response, local_ui_host.py

--- flask_sum.py
+++ flask_sum.py
@@ -1,6 +1,9 @@
 from flask import Flask, request
-
+import pandas as pd
+import requests
 app = Flask(__name__)
+
+   
 
 @app.route('/addtwointegers/<int:c>/<float:d>', methods=['POST'])
 def addtwo(c,d):
@@ -8,7 +11,15 @@ def addtwo(c,d):
   print(request.json)
   a = request.json.get('a')
   b = request.json.get('b')
-  return {"response": a+b+c+d}
+  request_to_save = {"a": a, "b": b}
+  data_pipeline_url = "http://127.0.0.1:5001/saveToCSV"
+  data_payload = request_to_save
+  data_req_save_response = requests.post(data_pipeline_url,json=data_payload )
+  print(data_req_save_response.content)
+  #save_into_csv(request_to_save)
+  output = {"response": a+b+c+d}
+  #save_response(output)
+  return output
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ local_ui_host.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import requests
+
+number1 = st.number_input("Enter the first number:")
+number2 = st.number_input("Enter the second number:")
+
+url = "http://127.0.0.1:5000/addtwointegers"
+testdata_c = 100
+testdata_d = 4.5
+
+url_1 = url + "/" + str(testdata_c) + "/" + str(testdata_d)
+print("Updated URl", url_1)
+
+data_payload = {"a": number1, "b": number2}
+output = requests.post(url=url_1, json=data_payload)
+output_dict = output.json()
+#print("response_code",output.response_code)
+sum_result = output_dict["response"]
+st.write("The sum is:", sum_result)
