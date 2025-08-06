4ede891552473f47c17552c6178019db304bb8bf
Sai sri ganesh Putsala <saiganesh.putsala@gmail.com>
2025-08-06 17:23:50 -0400

    made assert==actual in test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from sum import addtwo
 def test_addtwo_function(): # Renamed to follow 'test_' convention
     testdata_a = 20
     testdata_b = 30
-    expected = 40
+    expected = 50
     actual = addtwo(testdata_a, testdata_b)
 
     assert actual == expected
